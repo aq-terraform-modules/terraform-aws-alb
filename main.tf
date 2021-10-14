4bd50aa4113237b5fd9d925502f9291eb3b311e3
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 00:31:07 +0700

    Init commit for dev branch

---
+++ main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "lb_sg" {
+  name        = "${var.name}_lb_sg"
+  description = "Security Group for Load Balancer"
+
+  ingress = [
+    {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow HTTPS"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+  ]
+}
+
+
+resource "aws_lb" "core_lb" {
+  name                       = "${var.name}_lb"
+  internal                   = var.internal
+  load_balancer_type         = var.load_balancer_type
+  security_groups            = [aws_security_group.lb_sg.id]
+  subnets                    = var.subnets
+  enable_deletion_protection = var.enable_deletion_protection
+  ip_address_type            = var.ip_address_type
+
+  dynamic "subnet_mapping" {
+    for_each = var.subnet_mapping
+
+    content {
+      subnet_id            = subnet_mapping.value.subnet_id
+      allocation_id        = lookup(subnet_mapping.value, "allocation_id", null)
+      private_ipv4_address = lookup(subnet_mapping.value, "private_ipv4_address", null)
+      ipv6_address         = lookup(subnet_mapping.value, "ipv6_address", null)
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "name" {
+  description = "Name prefix"
+}
+
+variable "internal" {
+  description = "LB Internal or not"
+  type        = bool
+  default     = false
+}
+
+variable "load_balancer_type" {
+  description = "LB Type"
+  default     = "application"
+}
+
+variable "subnets" {
+  description = "List of Subnet IDs"
+  type        = list(any)
+  default     = []
+}
+
+variable "enable_deletion_protection" {
+  description = "Enable deletion protection or not"
+  type        = bool
+  default     = false
+}
+
+variable "ip_address_type" {
+  description = "IP Address Type of the LB"
+  default     = "ipv4"
+}
+
+variable "subnet_mapping" {
+  description = "Subnet for the LB"
+  type        = list(map(string))
+  default     = []
+}
