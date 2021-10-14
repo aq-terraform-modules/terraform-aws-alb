8a58fc5b257f57c3a0c29aa1a37f7116fca5efe5
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 01:01:09 +0700

    Test LB

--- main.tf
+++ main.tf
@@ -2,22 +2,21 @@ resource "aws_security_group" "lb_sg" {
   name        = "${var.name}_lb_sg"
   description = "Security Group for Load Balancer"
 
-  ingress = [
-    {
+  ingress = {
       description = "Allow HTTP"
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
+  }
+  
+  ingress = {
       description = "Allow HTTPS"
       from_port   = 443
       to_port     = 443
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-    },
-  ]
+  }
 }
 
 
